aeee3824a4bcb98f00dbf8711f10075823b32aa7
Jon Keam <jpkeam@gmail.com>
2024-08-17 01:18:30 -0400

    fixed tests

--- feedback/tests.py
+++ feedback/tests.py
@@ -12,43 +12,68 @@ def create_user(user_dict:dict):
         user.email = user_dict.get('email')
         user.first_name = user_dict.get('first_name')
         user.last_name = user_dict.get('last_name')
-        user.is_superuser = False
-        user.is_staff = False
+        user.is_superuser = user_dict.get('superuser', False)
+        user.is_staff = user_dict.get('superuser', False)
         user.save()
+
+def create_admin_user():
+    """ Create Admin user """
+    users = [{
+        'username': 'admin',
+        'password': 'admin1',
+        'first_name': 'Admin',
+        'last_name': 'Person',
+        'email': 'admin@example.com',
+        'superuser': True,
+    }]
+    for user in users:
+        create_user(user)
 
 def create_test_user():
     """ Create Foo user """
     users = [{
         'username': 'foo',
-        'password': 'bar',
+        'password': 'bar1',
         'first_name': 'Foo',
         'last_name': 'Bar',
         'email': 'foo@example.com',
-        }]
+        'superuser': False,
+    }]
     for user in users:
         create_user(user)
 
 def create_second_test_user():
+    """ Create Foo2 user """
     users = [{
         'username': 'foo2',
         'password': 'bar2',
         'first_name': 'Foo2',
         'last_name': 'Bar2',
         'email': 'foo2@example.com',
-        }]
+        'superuser': False,
+    }]
     for user in users:
         create_user(user)
 
-def get_user():
+def get_user(username:str = 'foo'):
     """ Get user """
     UserModel = get_user_model()
-    return UserModel.objects.filter(username='foo').first()
+    return UserModel.objects.filter(username=username).first()
 
-class IndexViewTests(TestCase):
-    # def setUp(self):
-        # self.user = create_test_user()
-        # self.client.login(username='foo', password='bar')
+class IndexUserViewTests(TestCase):
+    def setUp(self):
+        self.user = create_test_user()
+        self.client.login(username='foo', password='bar1')
 
     def test_index(self):
         response = self.client.get(reverse("feedback:root"))
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 302)
+
+class IndexAdminViewTests(TestCase):
+    def setUp(self):
+        self.user = create_admin_user()
+        self.client.login(username='admin', password='admin1')
+
+    def test_index(self):
+        response = self.client.get(reverse("feedback:root"))
+        self.assertEqual(response.status_code, 302)
