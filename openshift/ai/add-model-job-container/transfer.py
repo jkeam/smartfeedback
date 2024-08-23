4e3491fcb1503bc573dbd69150240838060652c4
Jon Keam <jpkeam@gmail.com>
2024-08-23 12:38:32 -0400

    add storage

--- openshift/ai/add-model-job-container/transfer.py
+++ openshift/ai/add-model-job-container/transfer.py
@@ -3,18 +3,26 @@ from os import getenv, listdir, path
 from os import listdir, path
 from subprocess import call
 
-call(["git", "clone", "https://huggingface.co/instructlab/granite-7b-lab", "/tmp/granite-7b-lab"])
+directory = "/tmp/models/granite-7b-lab"
+
+# download model
+call(["git", "clone", "https://huggingface.co/instructlab/granite-7b-lab", directory])
+print("model downloaded from huggingface")
 
 s3 = client("s3",
             endpoint_url=getenv("AWS_S3_ENDPOINT"),
             aws_access_key_id=getenv("AWS_ACCESS_KEY_ID"),
             aws_secret_access_key=getenv("AWS_SECRET_ACCESS_KEY"))
 
+# create bucket if not exist
 bucket = getenv("AWS_S3_BUCKET")
 if bucket not in [bu["Name"] for bu in s3.list_buckets()["Buckets"]]:
     s3.create_bucket(Bucket=bucket)
+    print(f"created {bucket} bucket")
+else:
+    print(f"{bucket} bucket exists already")
 
-directory = "/tmp/granite-7b-lab"
+# upload models
 for filename in listdir(directory):
     f = path.join(directory, filename)
     if path.isfile(f):
